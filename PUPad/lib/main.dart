84626155cea359bdd037703dfcc04ca1ebc6c67b
Romeo Rubio <romeocrubioiii@gmail.com>
2024-06-04 16:34:05 +0800

    Added the first version without biometrics

---
+++ PUPad/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:note_app/firebase_options.dart';
+import 'package:note_app/screens/signin.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignInScreen(),
+    );
+  }
+}
